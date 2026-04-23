ed4a21a404c2fb11a796fa7034ccaaeb3589da4c
Mattias Söderberg <mattes.email@gmail.com>
2026-04-23 18:13:57 +0200

    add encodeAndSave method to MessageService for encoding and saving messages

--- backend/src/main/java/se/salt/matte/backend/domain/services/MessageService.java
+++ backend/src/main/java/se/salt/matte/backend/domain/services/MessageService.java
@@ -1,14 +1,26 @@
 package se.salt.matte.backend.domain.services;
 
 import org.springframework.stereotype.Service;
+import se.salt.matte.backend.domain.models.Message;
+import se.salt.matte.backend.domain.models.Profile;
 import se.salt.matte.backend.domain.repositories.MessageRepository;
+
+import java.util.UUID;
 
 @Service
 public class MessageService {
 
     MessageRepository repository;
+    AudioSteganographyService audioSteganographyService;
 
-    public MessageService(MessageRepository repository) {
+    public MessageService(MessageRepository repository,
+                          AudioSteganographyService audioSteganographyService) {
         this.repository = repository;
+        this.audioSteganographyService = audioSteganographyService;
+    }
+
+    public Message encodeAndSave(UUID conversationId, String text, Profile sender) {
+        byte[] audioByteArray = audioSteganographyService.encode(text);
+        return null;
     }
 }
