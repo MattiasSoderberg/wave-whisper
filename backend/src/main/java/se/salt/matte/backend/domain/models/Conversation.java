8b71d7809a6114ce90d7da6d330ef68a6f232d53
Mattias Söderberg <mattes.email@gmail.com>
2026-04-23 13:35:03 +0200

    add Conversation entity to manage user interactions with timestamps

---
+++ backend/src/main/java/se/salt/matte/backend/domain/models/Conversation.java
@@ -0,0 +1,57 @@
+package se.salt.matte.backend.domain.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "conversations", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_a_id", "user_b_id"})
+})
+public class Conversation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_a_id", nullable = false)
+    Profile userA;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_b_id", nullable = false)
+    Profile userB;
+
+    @Column(updatable = false)
+    LocalDateTime createdAt = LocalDateTime.now();
+
+    public Conversation() {
+    }
+
+    public Conversation(Profile userA, Profile userB) {
+        if (userA.getId().compareTo(userB.getId()) < 0) {
+            this.userA = userA;
+            this.userB = userB;
+        } else {
+            this.userA = userB;
+            this.userB = userA;
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Profile getUserA() {
+        return userA;
+    }
+
+    public Profile getUserB() {
+        return userB;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
