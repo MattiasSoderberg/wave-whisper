a4b0e390648fcfa0a8cd250ecb9ee1092877bc95
Mattias Söderberg <mattes.email@gmail.com>
2026-04-23 13:40:02 +0200

    add Message entity to handle file messages within conversations

---
+++ backend/src/main/java/se/salt/matte/backend/domain/models/Message.java
@@ -0,0 +1,58 @@
+package se.salt.matte.backend.domain.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.UUID)
+    UUID id;
+
+    @ManyToOne()
+    @JoinColumn(name = "conversation_id", nullable = false)
+    Conversation conversation;
+
+    @ManyToOne()
+    @JoinColumn(name = "sender_id", nullable = false)
+    Profile sender;
+
+    @Column(nullable = false)
+    String filePath;
+
+    @Column(updatable = false)
+    LocalDateTime createdAt = LocalDateTime.now();
+
+    public Message() {
+    }
+
+    public Message(Conversation conversation, Profile sender, String filePath) {
+        this.conversation = conversation;
+        this.sender = sender;
+        this.filePath = filePath;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public Profile getSender() {
+        return sender;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
