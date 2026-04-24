51cdac970b9978e56acff857c55701364a6d2819
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 17:22:29 +0200

    refactor MessageService to inject MessageRepository and StorageService, update decode method to retrieve audio data by message ID

--- backend/src/main/java/se/salt/matte/backend/domain/services/MessageService.java
+++ backend/src/main/java/se/salt/matte/backend/domain/services/MessageService.java
@@ -1,17 +1,31 @@
 package se.salt.matte.backend.domain.services;
 
 import org.springframework.stereotype.Service;
+import se.salt.matte.backend.domain.models.Message;
+import se.salt.matte.backend.domain.repositories.MessageRepository;
+import se.salt.matte.backend.exception.MessageNotFoundException;
+
+import java.util.UUID;
 
 @Service
 public class MessageService {
 
+    private final MessageRepository messageRepository;
     private final AudioSteganographyService audioSteganographyService;
+    private final StorageService storageService;
 
-    public MessageService(AudioSteganographyService audioSteganographyService) {
+    public MessageService(
+            MessageRepository messageRepository,
+            AudioSteganographyService audioSteganographyService,
+            StorageService storageService) {
+        this.messageRepository = messageRepository;
         this.audioSteganographyService = audioSteganographyService;
+        this.storageService = storageService;
     }
 
-    public String decode(byte[] audioData) {
+    public String decode(UUID id) {
+        Message message = messageRepository.findById(id).orElseThrow(MessageNotFoundException::new);
+        byte[] audioData = storageService.downloadAudio(message.getFilePath());
         return audioSteganographyService.decode(audioData);
     }
 }
