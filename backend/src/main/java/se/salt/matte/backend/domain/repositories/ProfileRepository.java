2916ad7acfe73d2835c86734a8c22c5f03b635a4
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 15:49:10 +0200

    add method to find profile by email in ProfileRepository

--- backend/src/main/java/se/salt/matte/backend/domain/repositories/ProfileRepository.java
+++ backend/src/main/java/se/salt/matte/backend/domain/repositories/ProfileRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import se.salt.matte.backend.domain.models.Profile;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+    Optional<Profile> findByEmail(String email);
     List<Profile> findByUsernameContainingIgnoreCaseAndIdNot(String username, UUID id);
 }
