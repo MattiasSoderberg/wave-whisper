cc2a8046073e72cb6633bcf0c436470b168755ce
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 16:00:46 +0200

    add NotFoundException class for handling resource not found errors

---
+++ backend/src/main/java/se/salt/matte/backend/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package se.salt.matte.backend.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
