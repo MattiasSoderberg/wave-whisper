91807488fe125d2954d8e78ef630da95d875901f
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 17:12:14 +0200

    change MessageController from Controller to RestController for improved RESTful API support

--- backend/src/main/java/se/salt/matte/backend/web/controllers/MessageController.java
+++ backend/src/main/java/se/salt/matte/backend/web/controllers/MessageController.java
@@ -1,16 +1,16 @@
 package se.salt.matte.backend.web.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import se.salt.matte.backend.domain.services.MessageService;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 @RequestMapping("${spring.api.paths.messages}")
 public class MessageController {
 
