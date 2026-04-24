45f9efd8fd5b7669615d5e48ac26a3196a3b2226
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 16:19:48 +0200

    add ConversationController for managing conversation endpoints

---
+++ backend/src/main/java/se/salt/matte/backend/web/controllers/ConversationController.java
@@ -0,0 +1,35 @@
+package se.salt.matte.backend.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.salt.matte.backend.domain.models.Conversation;
+import se.salt.matte.backend.domain.services.ConversationService;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("${spring.api.paths.conversations}")
+public class ConversationController {
+
+    private final ConversationService conversationService;
+
+    public ConversationController(ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Conversation> startConversation(Principal principal, @RequestParam UUID receiverId) {
+        Conversation conversation = conversationService.startConversation(
+                principal.getName(), receiverId
+        );
+        return ResponseEntity.ok(conversation);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Conversation>> getUserConversations(Principal principal) {
+        List<Conversation> conversations = conversationService.getUserConversations(principal.getName());
+        return ResponseEntity.ok(conversations);
+    }
+}
