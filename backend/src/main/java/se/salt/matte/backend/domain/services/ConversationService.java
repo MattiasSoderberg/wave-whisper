f1656f20e8072a12eab1fbcdb51d438ffa97286d
Mattias Söderberg <mattes.email@gmail.com>
2026-04-24 16:01:04 +0200

    add ConversationService for managing conversations between profiles

---
+++ backend/src/main/java/se/salt/matte/backend/domain/services/ConversationService.java
@@ -0,0 +1,34 @@
+package se.salt.matte.backend.domain.services;
+
+import org.springframework.stereotype.Service;
+import se.salt.matte.backend.domain.models.Conversation;
+import se.salt.matte.backend.domain.models.Profile;
+import se.salt.matte.backend.domain.repositories.ConversationRepository;
+import se.salt.matte.backend.domain.repositories.ProfileRepository;
+import se.salt.matte.backend.exception.ProfileNotFoundException;
+
+import java.util.UUID;
+
+@Service
+public class ConversationService {
+
+    private final ConversationRepository conversationRepository;
+    private final ProfileRepository profileRepository;
+
+    public ConversationService(ConversationRepository conversationRepository,
+                               ProfileRepository profileRepository) {
+        this.conversationRepository = conversationRepository;
+        this.profileRepository = profileRepository;
+    }
+
+    public Conversation startConversation(UUID senderId, UUID receiverId) {
+        Profile sender = profileRepository.findById(senderId).orElseThrow(ProfileNotFoundException::new);
+        Profile receiver = profileRepository.findById(receiverId).orElseThrow(ProfileNotFoundException::new);
+
+        Conversation tempConversation = new Conversation(sender, receiver);
+
+        return conversationRepository.findByUserAAndUserB(
+                tempConversation.getUserA(), tempConversation.getUserB()
+        ).orElseGet(() -> conversationRepository.save(tempConversation));
+    }
+}
