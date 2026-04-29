837a31e1d218be687ff386e716cb4a2f8db8a768
Mattias Söderberg <mattes.email@gmail.com>
2026-04-29 20:56:07 +0200

    fix: add action field to ConversationCreatedEvent for event tracking

--- backend/src/main/java/se/salt/matte/backend/domain/event/ConversationCreatedEvent.java
+++ backend/src/main/java/se/salt/matte/backend/domain/event/ConversationCreatedEvent.java
@@ -5,12 +5,18 @@ import se.salt.matte.backend.domain.models.Conversation;
 public class ConversationCreatedEvent {
 
     private final Conversation conversation;
+    private final String action;
 
     public ConversationCreatedEvent(Conversation conversation) {
         this.conversation = conversation;
+        action = "CREATED";
     }
 
     public Conversation getConversation() {
         return conversation;
+    }
+
+    public String getAction() {
+        return action;
     }
 }
