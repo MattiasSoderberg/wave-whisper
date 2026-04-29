72dce99371bdc527e380d903de5d1ff43e63fcac
Mattias Söderberg <mattes.email@gmail.com>
2026-04-29 15:18:34 +0200

    fix: update WebSocketConfig to use allowed origins from configuration

--- backend/src/main/java/se/salt/matte/backend/config/WebSocketConfig.java
+++ backend/src/main/java/se/salt/matte/backend/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package se.salt.matte.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,11 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${spring.allowed.origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*");
-//                .withSockJS();
+                .setAllowedOrigins(allowedOrigins);
     }
 
     @Override
